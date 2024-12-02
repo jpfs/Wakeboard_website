b1b45f1fd357832444da5bbcd01ff38a783a437c
Joao Santos <jp.f.s@hotmail.com>
2024-12-02 17:04:16 +0300

    commit

--- header.js
+++ header.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("header").innerHTML = `
     <nav class="main-nav">
       <a href="home.html">Home</a>
-      <a href="history.html">History</a>
-      <a href="get_start.html">Get Start</a>
+      <a href="get_start.html">Get Start</a> 
       <a href="tricks_tutorials.html">Tricks tutorials</a>
+      <a href="history.html">History</a>
     </nav>
   `;
 
